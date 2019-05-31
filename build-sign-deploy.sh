785305559f072c90b8b75cc9471ecc69ec51f029
Phillip <phillip.shipley@gmail.com>
2019-05-31 16:12:32 -0400

    generate base64 encoded sha256 checksum for terraform to use

--- build-sign-deploy.sh
+++ build-sign-deploy.sh
@@ -7,9 +7,13 @@ set -x -e
 
 GOOS=linux go build -ldflags="-s -w" -o idp-id-broker-search main.go
 zip idp-id-broker-search.zip idp-id-broker-search
+
+# Create base64 encoded sha256 checksum for terraform to use to detect changes
+sha256sum idp-id-broker-search.zip | base64 > idp-id-broker-search.zip.sum
+
 #runny gpg --yes -a -o "idp-id-broker-search.zip.sig" --detach-sig idp-id-broker-search.zip
 
-
-# Push zip to S3 under folder for CI_BRANCH (ex: develop or 1.2.3)
+# Push zip and checksum to S3 under folder for CI_BRANCH (ex: develop or 1.2.3)
 CI_BRANCH=${CI_BRANCH:="unknown"}
 aws s3 cp --acl public-read idp-id-broker-search.zip s3://$DOWNLOAD_BUCKET/$CI_BRANCH/
+aws s3 cp --acl public-read idp-id-broker-search.zip.sum s3://$DOWNLOAD_BUCKET/$CI_BRANCH/
