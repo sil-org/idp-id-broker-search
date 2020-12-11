e4453ff2a69a0ab390c805d2ab9bf11cba484971
Schparky <3172830+Schparky@users.noreply.github.com>
2020-12-11 15:42:17 -0700

    upgrade terraform to 0.13

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,5 @@
 provider "aws" {
-  version    = "~> 2.70"
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-}
-
-provider "template" {
-  version = "~> 2.2"
-}
-
-provider "null" {
-  version = "~> 3.0"
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      version = "~> 2.70"
+      source  = "hashicorp/aws"
+    }
+    null = {
+      version = "~> 3.0"
+      source  = "hashicorp/null"
+    }
+    template = {
+      version = "~> 2.2"
+      source  = "hashicorp/template"
+    }
+  }
 }
