77ccca380446237a0fb251fc294493c7e12f9fb6
Schparky <3172830+Schparky@users.noreply.github.com>
2021-05-05 09:51:33 -0600

    need credentials on second provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,8 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "region-2"
-  region = local.aws-region-2
+  alias      = region-2
+  region     = local.aws-region-2
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
