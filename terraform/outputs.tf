d06473536fda6b9876c295d7b4ba72101883a71f
briskt <3172830+briskt@users.noreply.github.com>
2024-09-17 19:39:54 +0800

    use new names in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,12 @@
 output "ci-uploader-access-key" {
-  value = aws_iam_access_key.ci-uploader.id
+  value = aws_iam_access_key.ci_uploader.id
 }
 
 output "ci-uploader-secret-key" {
-  value     = aws_iam_access_key.ci-uploader.secret
+  value     = aws_iam_access_key.ci_uploader.secret
   sensitive = true
 }
 
 output "bucket-name" {
-  value = aws_s3_bucket.idp-id-broker-search.bucket
+  value = aws_s3_bucket.idp_id_broker_search.bucket
 }
