1cdef74ad1dc4c012e33e6e1eef96d44c5c9dad5
briskt <3172830+briskt@users.noreply.github.com>
2024-11-11 22:19:42 +0800

    remove the public-read ACL on the deployment

--- build-sign-deploy.sh
+++ build-sign-deploy.sh
@@ -11,8 +11,8 @@ openssl dgst -binary -sha256 idp-id-broker-search.zip | base64 --wrap=0 > idp-id
 # Push zip and checksum to S3 under folder for GITHUB_REF_NAME (ex: develop or 1.2.3)
 GITHUB_REF_NAME=${GITHUB_REF_NAME:="unknown"}
 bucket=$DOWNLOAD_BUCKET-${AWS_REGION}
-aws s3 cp --acl public-read idp-id-broker-search.zip s3://$bucket/$GITHUB_REF_NAME/
-aws s3 cp --acl public-read --content-type text/plain idp-id-broker-search.zip.sum s3://$bucket/$GITHUB_REF_NAME/
+aws s3 cp idp-id-broker-search.zip s3://$bucket/$GITHUB_REF_NAME/
+aws s3 cp --content-type text/plain idp-id-broker-search.zip.sum s3://$bucket/$GITHUB_REF_NAME/
 
 if [ -z $AWS_REGION2 ]; then
   exit 0
@@ -20,5 +20,5 @@ fi
 
 export AWS_REGION=${AWS_REGION2}
 bucket=$DOWNLOAD_BUCKET-${AWS_REGION}
-aws s3 cp --acl public-read idp-id-broker-search.zip s3://$bucket/$GITHUB_REF_NAME/
-aws s3 cp --acl public-read --content-type text/plain idp-id-broker-search.zip.sum s3://$bucket/$GITHUB_REF_NAME/
+aws s3 cp idp-id-broker-search.zip s3://$bucket/$GITHUB_REF_NAME/
+aws s3 cp --content-type text/plain idp-id-broker-search.zip.sum s3://$bucket/$GITHUB_REF_NAME/
