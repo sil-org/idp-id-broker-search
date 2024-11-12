1ecc3ff05e9778afa6192414526a2a98f9561a20
briskt <3172830+briskt@users.noreply.github.com>
2024-11-12 16:45:17 +0800

    refactor based on Sonar warning

--- main_test.go
+++ main_test.go
@@ -137,11 +137,12 @@ func Test_search(t *testing.T) {
 			mux.HandleFunc("/user", func(w http.ResponseWriter, req *http.Request) {
 				w.WriteHeader(tt.args.status)
 				w.Header().Set("content-type", "application/json")
-				if tt.args.status == http.StatusOK {
-					_, err := fmt.Fprint(w, tt.args.jsonResponse)
-					if err != nil {
-						require.NoError(t, err)
-					}
+				if tt.args.status != http.StatusOK {
+					return
+				}
+				_, err := fmt.Fprint(w, tt.args.jsonResponse)
+				if err != nil {
+					require.NoError(t, err)
 				}
 			})
 
